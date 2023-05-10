64c21243257700f5e2eadbf6991982d954950541
Your Name <you@example.com>
2023-05-10 23:27:10 +0800

    a

--- modules/codeformer_model.py
+++ modules/codeformer_model.py
@@ -15,7 +15,7 @@ from modules.paths import models_path
 # I am making a choice to include some files from codeformer to work around this issue.
 model_dir = "Codeformer"
 model_path = os.path.join(models_path, model_dir)
-model_url = 'https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth'
+model_url = 'https://ghproxy.com/https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth'
 
 have_codeformer = False
 codeformer = None
